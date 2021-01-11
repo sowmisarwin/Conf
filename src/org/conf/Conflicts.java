27cebdfb7259ec2f6a95e47d2e705e37fd271a2d
sowmisarwin <sowmimca9297@gmail.com>
2021-01-11 23:06:09 +0530

    update

--- src/org/conf/Conflicts.java
+++ src/org/conf/Conflicts.java
@@ -4,6 +4,9 @@ public class Conflicts {
 public static void main(String[] args) {
 	System.out.println("first sample pgm");
 	System.out.println("out");
+	int a=10;
+	int b=20;
+	System.out.println(a+b);
 	
 	
 }
